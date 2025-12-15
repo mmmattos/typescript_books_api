24cbdb5e8bb8b3a1c415991263dc72fd9689b11f
Miguel Miranda de Mattos <mmmattos@gmail.com>
2025-12-15 11:38:14 -0300

    feat/scaffold nestjs api. added users module, controller, service

---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService, User } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  findAll(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): User | undefined {
+    return this.usersService.findOne(Number(id));
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    { id: 1, name: 'Alice', email: 'alice@test.com' },
+    { id: 2, name: 'Bob', email: 'bob@test.com' },
+  ];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number): User | undefined {
+    return this.users.find(user => user.id === id);
+  }
+}
